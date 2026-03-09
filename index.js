bf31d109fad8840d81eebac43c4379653093205d
Andrey Mironov <a.mironov@lucky-team.pro>
2026-03-09 20:49:38 +0700

    got token

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const apiHash = 'f8996a235b5f84fa191db9ac2323fd32'
 const phone = '+375257151409'
 
 // 👉 сюда потом вставишь сохранённую сессию
-const stringSession = new StringSession('')
+const stringSession = new StringSession(
+  '1AgAOMTQ5LjE1NC4xNjcuNDEBu6Kpe/7P9+eOuFZjZ5C6L7IEr+oG5gpSE5RU/FhQ8dmBoDvmeaESlhkJbpx9r2Ra46xB5SIB2ePCFUiMAUzj5AP1zVzu2mba661cDWYjv63q1VPh8ebxuJnMTFtku3U6p2G68EWGD80dtBY5xl9DD2VINB4FJ++WsBPR75fexqGsHrD+L8daVxRDPFlt1hWu5RqTMyQjYWC3qpA+3/LIt5g8Y+BVEyA4MPIH30TbNMRg73Ny9dJ1p00i1TMY7Yh+cyes9TJpPTXiAmRmG1rKBRFEdtGcxkYv8qPKUbLLnpUpOXiODIlfyNOoXqO7wGx94CXQz3w10KsmCPRp1lPHaqQ=',
+)
 
 const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 })
 
